fab8d408b40cd97f70c98aede88363048f7ebc8f
Sebastian <sebekk7@gmail.com>
2021-12-20 19:49:56 +0100

    edycja metody usuwajacej biale znaki i zamieniajacej pierwsze litery na wielkie

--- src/main/java/BookValidation.java
+++ src/main/java/BookValidation.java
@@ -2,14 +2,15 @@ public class BookValidation {
 
     public Book isBookCorrectly(Book book) {
 
-        String titleTrim = book.getTitle().trim();
-        String firstTitleLetterToUpperCase = titleTrim.substring(0, 1).toUpperCase() + titleTrim.substring(1);
-        book.setTitle(firstTitleLetterToUpperCase);
+        isWordOk(book.getTitle());
+        isWordOk(book.getAuthor());
 
-        String authorTrim = book.getAuthor().trim();
-        String firstAuthorLetterToUpperCase = authorTrim.substring(0, 1).toUpperCase() + authorTrim.substring(1);
-        book.setAuthor(firstAuthorLetterToUpperCase);
-
-        return new Book(book.getId(), book.getTitle(), book.getAuthor());
+        return new Book(book.getId(), isWordOk(book.getTitle()), isWordOk(book.getAuthor()));
     }
+
+    public String isWordOk(String word) {
+        String wordCorrectly = word.trim();
+        return wordCorrectly.substring(0, 1).toUpperCase() + wordCorrectly.substring(1);
+    }
+
 }
